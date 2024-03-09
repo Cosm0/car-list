4f653297869178734e5390242b0fa6161f2b80a8
Przemysław Podsiadlik <ppodsiadlik@future-processing.com>
2024-03-09 18:45:54 +0100

    Shared module with config

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,18 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
-    console.log(`Server listening on port 3000`);
+    const configService = app.get(ConfigService);
+    const port = configService.get('port');
+
+    await app.listen(port);
+    console.log(`Server listening on port ${port}`);
   } catch (err) {
     console.log(err);
     process.exit();
   }
 }
 bootstrap();
-
 
 process.on('SIGTERM', () => {
   console.log('Received SIGTERM. Gracefully shutting down...');
