07a01a8f39b06bb92b2462280a25ee942c06fcb9
Przemysław Podsiadlik <ppodsiadlik@future-processing.com>
2024-03-09 19:55:30 +0100

    Connect api via typeorm to db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [SharedModule],
+  imports: [
+    SharedModule,
+    TypeOrmModule.forRootAsync({
+      imports: [SharedModule, ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const host = configService.get('db.host');
+        const database = configService.get('db.database');
+        const username = configService.get('db.user');
+        const password = configService.get('db.password');
+
+        return {
+          type: 'mysql',
+          host,
+          database,
+          username,
+          password,
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/shared/config.ts
+++ api/src/shared/config.ts
@@ -1,7 +1,8 @@
 export default () => ({
   port: process.env.PORT || 3000,
   db: {
-    host: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
   },
