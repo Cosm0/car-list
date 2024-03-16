83ad75ad3ba71134e54af687a7ccac8d9557d276
Przemysław Podsiadlik <ppodsiadlik@future-processing.com>
2024-03-16 10:16:15 +0100

    Fixes Vehicles module - adds missing Entity registration

--- api/src/vehicles/vehicles.module.ts
+++ api/src/vehicles/vehicles.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { VehiclesController } from './vehicles.controller';
 import { VehiclesService } from './vehicles.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Vehicle } from 'src/typeorm/entities/Vehicle';
+import { Renter } from 'src/typeorm/entities/Renter';
 
 @Module({
   controllers: [VehiclesController],
   providers: [VehiclesService],
-  imports: [TypeOrmModule.forFeature([Vehicle])],
+  imports: [TypeOrmModule.forFeature([Vehicle, Renter])],
 })
 export class VehiclesModule {}
