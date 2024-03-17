7d011432da162388fbe84b9332dde00000b0d35b
Przemysław Podsiadlik <ppodsiadlik@future-processing.com>
2024-03-17 10:29:34 +0100

    Fixes config in API

--- api/src/shared/config.ts
+++ api/src/shared/config.ts
@@ -1,7 +1,7 @@
 export default () => ({
   port: process.env.PORT || 3000,
   node_env: process.env.NODE_ENV,
-  isProduction: process.env.NODE_ENV === 'production',- change longitude/latitude types from numbers to GeoJSON 
+  isProduction: process.env.NODE_ENV === 'production', // change longitude/latitude types from numbers to GeoJSON 
 
   db: {
     host: process.env.MYSQL_HOST,
